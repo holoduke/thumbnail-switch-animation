9213d568be8bdd497355735c0cb5c520ebddd6d0
Gillis Haasnoot <gillis.haasnoot@gmail.com>
2014-09-08 14:15:50 +0200

    added different ready functiion, possible to init during runtime

--- thumb_switcher.js
+++ thumb_switcher.js
@@ -1,5 +1,37 @@
 (function(){
 	
+	var elEventListeners = [];
+	
+	function addEventListeners(el,mouseover,mouseout){
+		console.log('add event ',el)
+		elEventListeners.push({'el':el,'mouseover':mouseover,'mouseout':mouseout});
+		el.addEventListener("mouseover", mouseover);	
+		el.addEventListener("mouseout", mouseout);
+	}
+	
+	function removeEventListeners(el){
+		
+		
+		for(var i=elEventListeners.length-1; i >=0 ;i--){
+			
+			if (elEventListeners[i].el == el){
+				el.removeEventListener("mouseover",elEventListeners[i].mouseover);
+				el.removeEventListener("mouseout",elEventListeners[i].mouseout);
+				
+				elEventListeners.splice(i,1);
+			}
+		}	
+	}
+	
+	function reset(){
+		for(var i=elEventListeners.length-1; i >=0 ;i--){
+			console.log(elEventListeners[i]);
+			elEventListeners[i].el.removeEventListener("mouseover",elEventListeners[i].mouseover);
+			elEventListeners[i].el.removeEventListener("mouseout",elEventListeners[i].mouseout);
+			elEventListeners.splice(i,1);
+		}		
+	}
+	
 	function init(el){
 		var switchSpeed = el.getAttribute("data-thumb-switch-speed");
 		var alternativeHost = el.getAttribute("data-thumb-host");
@@ -62,8 +94,9 @@
 					
 					//no roting images found at all
 					if (currentImage ==1){
-						el.removeEventListener("mouseover",mouseoverEvent);
-						el.removeEventListener("mouseout",mouseoutEvent);
+						removeEventListeners(el);
+						//el.removeEventListener("mouseover",mouseoverEvent);
+						//el.removeEventListener("mouseout",mouseoutEvent);
 					}
 					else{
 						limit = currentImage; //make sure next time we don't try to load the nonexisting image
@@ -84,20 +117,98 @@
 			el.src = orgSrc;	
 			currentImage = 1;
 		}
-		
-		el.addEventListener("mouseover", mouseoverEvent);	
-		el.addEventListener("mouseout", mouseoutEvent);
+		addEventListeners(el,mouseoverEvent,mouseoutEvent)
+		//el.addEventListener("mouseover", mouseoverEvent);	
+		//el.addEventListener("mouseout", mouseoutEvent);
 	}
-
-	ready = function(f){/in/.test(document.readyState) ? setTimeout(function(){ f()},9):f()}
 	
-	ready(function(){
+	window.ThumbSwitcher = {};
+	window.ThumbSwitcher.init = function(){
+		
+		reset();
 		var imageList = document.getElementsByClassName("thumb_switcher");
-		 
+
 		var len=imageList.length;
 		for (i=0; i < len; i++){
-			
 			init(imageList[i]);
-		}
-	});	
+		}		
+	}
+	
+	window.ThumbSwitcher.reset = function(){
+		reset();
+	}
+	
+	var domReady = (function() {
+
+	    var w3c = !!document.addEventListener,
+	        loaded = false,
+	        toplevel = false,
+	        fns = [];
+	    
+	    if (w3c) {
+	        document.addEventListener("DOMContentLoaded", contentLoaded, true);
+	        window.addEventListener("load", ready, false);
+	    }
+	    else {
+	        document.attachEvent("onreadystatechange", contentLoaded);
+	        window.attachEvent("onload", ready);
+	        
+	        try {
+	            toplevel = window.frameElement === null;
+	        } catch(e) {}
+	        if ( document.documentElement.doScroll && toplevel ) {
+	            scrollCheck();
+	        }
+	    }
+	
+	    function contentLoaded() {
+	        (w3c)?
+	            document.removeEventListener("DOMContentLoaded", contentLoaded, true) :
+	            document.readyState === "complete" && 
+	            document.detachEvent("onreadystatechange", contentLoaded);
+	        ready();
+	    }
+	    
+	    // If IE is used, use the trick by Diego Perini
+	    // http://javascript.nwbox.com/IEContentLoaded/
+	    function scrollCheck() {
+	        if (loaded) {
+	            return;
+	        }
+	        
+	        try {
+	            document.documentElement.doScroll("left");
+	        }
+	        catch(e) {
+	            window.setTimeout(arguments.callee, 15);
+	            return;
+	        }
+	        ready();
+	    }
+	    
+	    function ready() {
+	        if (loaded) {
+	            return;
+	        }
+	        loaded = true;
+	        
+	        var len = fns.length,
+	            i = 0;
+	            
+	        for( ; i < len; i++) {
+	            fns[i].call(document);
+	        }
+	    }
+	    
+	    return function(fn) {
+	        // if the DOM is already ready,
+	        // execute the function
+	        return (loaded)? 
+	            fn.call(document):      
+	            fns.push(fn);
+	    }
+	})();
+
+	domReady(function(){window.ThumbSwitcher.init();})
+	
 })();
